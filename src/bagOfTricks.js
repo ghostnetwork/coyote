8a1d3ceabb035a67989bc7f7100d2244190cf4a1
Code Swimmer <codeswimmer@gmail.com>
2013-08-27 16:32:37 -0700

    Fix colorWithAlpha
    It now converts each of the individual color values to hex.

--- src/bagOfTricks.js
+++ src/bagOfTricks.js
@@ -1,8 +1,8 @@
 
 function colorWithAlpha(color, alpha) {
   // Assumes color has format of '#RRGGBB'
-  var red = color.substring(1, 3);
-  var green = color.substring(3, 5);
-  var blue = color.substring(5, 7);
+  var red = parseInt(color.substring(1, 3), 16);
+  var green = parseInt(color.substring(3, 5), 16);
+  var blue = parseInt(color.substring(5, 7), 16);
   return 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha +')';
 }
